63e4ee8de5bd3f317419b46f1d3b2323cd48bc3b
Souradip Mookerjee <souramoo@gmx.com>
2021-01-30 04:24:16 +0000

    padding fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Skeleton from "react-loading-skeleton";
 
 export default function App() {
   const [text, setText] = useState([]);
-  const metadata = ["Souradip Mookerjee", "MB/PhD Student", "face.jpg"];
+  const metadata = ["Souradip Mookerjee", "MB/PhD Student", "face.jpg?s"];
 
   const img = new Image();
   img.onload = () => {

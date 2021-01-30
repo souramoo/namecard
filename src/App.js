cb41c4499ff29e9a96f8a7285fc84eaf77a4e0f4
Souradip Mookerjee <souramoo@gmx.com>
2021-01-30 15:34:56 +0000

    add app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Skeleton from "react-loading-skeleton";
 
-function App() {
+export default function App() {
+  const [text, setText] = useState([]);
+  const metadata = ["Souradip Mookerjee", "MB/PhD Student", "face.jpg?s"];
+
+  const img = new Image();
+  img.onload = () => {
+    setTimeout(() => setText(metadata), 1000);
+  };
+  img.src = metadata[2];
+
+  useEffect(() => {
+        var lockFunction =  window.screen.orientation.lock;
+        try {
+        if (lockFunction.call(window.screen.orientation, 'landscape')) {
+                console.log('Orientation locked')
+              } else {
+                  console.error('There was a problem in locking the orientation')
+        }
+      } catch {}
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="face" style={{ backgroundImage: "url(" + text[2] + ")" }}>
+        {text[2] ? (
+          ""
+        ) : (
+          <Skeleton circle={true} height={"100%"} width={"100%"} />
+        )}
+      </div>
+      <div className="text">
+        <h1>{text[0] || <Skeleton width={330} />}</h1>
+        <span className="subtext">{text[1] || <Skeleton width={200} />}</span>
+      </div>
     </div>
   );
 }
-
-export default App;

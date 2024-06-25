923f1506102f67f9c820f36a651f54bf31222fd9
Souradip Mookerjee <souramoo@gmx.com>
2024-06-26 00:58:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Skeleton from "react-loading-skeleton";
 
 export default function App() {
   const [text, setText] = useState([]);
-  const metadata = ["Souradip Mookerjee", "MB/PhD Student", "face.jpg?s"];
+  const metadata = ["Souradip Mookerjee", "MA MB BChir PhD (Cantab)", "face.jpg?s"];
 
   const img = new Image();
   img.onload = () => {
-    setTimeout(() => setText(metadata), 1000);
+    setTimeout(() => setText(metadata), 500);
   };
   img.src = metadata[2];
 
